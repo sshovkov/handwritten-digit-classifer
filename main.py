0c8821009b8e9cc6f52ac882e9218eebb83afe92
sshovkov <sophiashovkovy@gmail.com>
2023-09-20 16:29:19 -0400

    prediction is accurate

--- main.py
+++ main.py
@@ -3,44 +3,76 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
 
-# Load the MNIST training data using load_mnist module
-train_images = load_mnist.train_images
-train_labels = load_mnist.train_labels
 
-# Load the MNIST testing data using load_mnist module
-test_images = load_mnist.test_images
-test_labels = load_mnist.test_labels
+def main():
+    # Load and process a local image
+    image_path = "local_test_image/image_7.jpg"
+    img = load_and_process_image(image_path)
 
-# Create a KNN classifier
-handwritten_digit_model = KNeighborsClassifier(n_neighbors=3)
+    # Load the MNIST training data
+    train_images = load_mnist.train_images
+    train_labels = load_mnist.train_labels
 
-# Train the model on the training data
-# train_labels is the prediction target
-# train_images is the features
-handwritten_digit_model.fit(train_images, train_labels)
+    # Create a KNN classifier
+    handwritten_digit_model = KNeighborsClassifier(n_neighbors=3)
 
-# Make predictions on the test data
-predictions = handwritten_digit_model.predict(test_images)
+    # Train the model on the training data
+    handwritten_digit_model.fit(train_images, train_labels)
 
-# Calculate accuracy
-accuracy = accuracy_score(test_labels, predictions)
-print(f"Model Accuracy: {accuracy * 100:.2f}%")
+    # Predict handwritten digit in local image
+    prediction = handwritten_digit_model.predict(img)
 
-# Load and process a local image
-image_path = "local_test_image/image_3.jpg"
-img = Image.open(image_path).convert("L")  # Convert to grayscale
-img = Image.fromarray(255 - np.array(img))  # Invert the colors
-img = img.resize((28, 28))  # Resize to match MNIST image size
-img = np.array(img).reshape(1, -1)  # (1, -1) for single image
+    # Get the indices of the nearest neighbors
+    nearest_neighbors_indices = handwritten_digit_model.kneighbors(
+        img, n_neighbors=3, return_distance=False
+    )
 
-# Make prediction on local image
-prediction = handwritten_digit_model.predict(img)
-print(f"Classification of local image: {prediction[0]}")
+    # Retrieve the nearest neighbor images
+    nearest_neighbor_images = train_images[nearest_neighbors_indices[0]]
 
-# Display the local image and prediction
-plt.imshow(img.reshape(28, 28), cmap=plt.cm.gray_r)
-plt.title(f"Predicted Digit: {prediction[0]}")
-plt.axis("off")
-plt.show()
+    # Display the local image and its nearest neighbors
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 4, 1)
+    plt.imshow(img.reshape(28, 28), cmap=plt.cm.gray, vmin=0, vmax=255)
+    plt.title(f"Local Image\nPredicted Digit: {prediction[0]}")
+    plt.axis("off")
+
+    for i, neighbor_index in enumerate(nearest_neighbors_indices[0]):
+        plt.subplot(1, 4, i + 2)
+        plt.imshow(
+            nearest_neighbor_images[i].reshape(28, 28),
+            cmap=plt.cm.gray,
+            vmin=0,
+            vmax=255,
+        )
+        plt.title(f"Neighbor {i + 1}\nTrue Digit: {train_labels[neighbor_index]}")
+        plt.axis("off")
+
+    plt.tight_layout()
+    plt.show()
+
+
+def load_and_process_image(image_path: str) -> np.ndarray:
+    img = Image.open(image_path).convert("L")  # Convert to grayscale
+    img = Image.fromarray(255 - np.array(img))  # Invert the colors
+    img = img.resize((28, 28))  # Resize to match MNIST image size
+    img = np.array(img).reshape(1, -1)
+
+    # Normalize local image colors
+    return scale_image_colors(img)
+
+
+def scale_image_colors(img: np.ndarray) -> np.ndarray:
+    """
+    Scale the colors of an image to be between 0 and 255 for accurate model prediction.
+    """
+    numpy_min = np.min(img)
+    numpy_max = np.max(img)
+    numpy_diff = numpy_max - numpy_min
+    img = (img - numpy_min) * (255 / numpy_diff)
+    return img
+
+
+if __name__ == "__main__":
+    main()
